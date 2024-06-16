e3b5b7f866e5373141b3fd5b97a43b642e22cbf6
Jeremy Lewi <83630070+JeremyLewi@users.noreply.github.com>
2024-06-17 03:00:59 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: 'https://backend-production-1a8a.up.railway.app', //
+		origin: '*', //
+		credentials: false,
 	})
 );
 
